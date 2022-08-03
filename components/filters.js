e40961cba7d7169a2e8cfcf6e4de95c7b4f9924f
cutlope <abdullah77@outlook.com>
2022-08-04 03:29:50 +0500

    added filters

---
+++ components/filters.js
@@ -0,0 +1,86 @@
+const filters = [
+  {
+    id: "color",
+    name: "Color",
+    options: [
+      { value: "white", label: "White" },
+      { value: "beige", label: "Beige" },
+      { value: "blue", label: "Blue" },
+      { value: "brown", label: "Brown" },
+      { value: "green", label: "Green" },
+      { value: "purple", label: "Purple" },
+    ],
+  },
+  {
+    id: "category",
+    name: "Category",
+    options: [
+      { value: "new-arrivals", label: "All New Arrivals" },
+      { value: "tees", label: "Tees" },
+      { value: "crewnecks", label: "Crewnecks" },
+      { value: "sweatshirts", label: "Sweatshirts" },
+      { value: "pants-shorts", label: "Pants & Shorts" },
+    ],
+  },
+  {
+    id: "sizes",
+    name: "Sizes",
+    options: [
+      { value: "xs", label: "XS" },
+      { value: "s", label: "S" },
+      { value: "m", label: "M" },
+      { value: "l", label: "L" },
+      { value: "xl", label: "XL" },
+      { value: "2xl", label: "2XL" },
+    ],
+  },
+];
+
+
+export default function Filters() {
+  return (
+    <div className="bg-white">
+      <div>
+        <main className="max-w-2xl px-4 py-16 mx-auto sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+          <div className="pt-12 lg:grid lg:grid-cols-3 lg:gap-x-8 xl:grid-cols-4">
+            <aside>
+              <div className=" lg:block">
+                <form className="space-y-10 divide-y divide-gray-200">
+                  {filters.map((section, sectionIdx) => (
+                    <div
+                      key={section.name}
+                      className={sectionIdx === 0 ? null : "pt-10"}>
+                      <fieldset>
+                        <legend className="block text-sm font-medium text-gray-900">{section.name}</legend>
+                        <div className="pt-6 space-y-3">
+                          {section.options.map((option, optionIdx) => (
+                            <div
+                              key={option.value}
+                              className="flex items-center">
+                              <input
+                                id={`${section.id}-${optionIdx}`}
+                                name={`${section.id}[]`}
+                                defaultValue={option.value}
+                                type="checkbox"
+                                className="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500"
+                              />
+                              <label
+                                htmlFor={`${section.id}-${optionIdx}`}
+                                className="ml-3 text-sm text-gray-600">
+                                {option.label}
+                              </label>
+                            </div>
+                          ))}
+                        </div>
+                      </fieldset>
+                    </div>
+                  ))}
+                </form>
+              </div>
+            </aside>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
