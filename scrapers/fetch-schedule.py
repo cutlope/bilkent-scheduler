1966bf46798c8d545c09bf8aa13af93d2bcdd4b7
cutlope <abdullah77@outlook.com>
2022-08-11 21:24:38 +0500

    refactor

--- scrapers/fetch-schedule.py
+++ scrapers/fetch-schedule.py
@@ -10,16 +10,20 @@ with open(file_path, 'r') as f:
 
 semesterCode = semesters[0].get("code")
 
-file_path = (base_path / "../data/courses.json").resolve()
+file_path = (base_path / "../data/courses-extra.json").resolve()
 with open(file_path, 'r') as f:
     allCourses = json.load(f)
 
-
 for depart in allCourses:
-    for courseCode in allCourses[depart]:
-        for sectionNo in allCourses[depart][courseCode].get("section"):
-            sectionCode = courseCode + "-" + sectionNo
-            url = "https://stars.bilkent.edu.tr/homepage/ajax/schedule.php?COURSE={sectionCode}&SEMESTER={semesterCode}".format(sectionCode=sectionCode, semesterCode=semesterCode)
+    departCode = list(depart.keys())[0]
+    for course in depart[departCode]["courses"]:
+        courseCode = course["code"]
+        courseName = course["name"]
+        for section in course["sections"]:
+            sectionNumber = section["number"]
+            sectionCode = courseCode + "-" + sectionNumber
+            url = "https://stars.bilkent.edu.tr/homepage/ajax/schedule.php?COURSE={sectionCode}&SEMESTER={semesterCode}".format(
+                sectionCode=sectionCode, semesterCode=semesterCode)
             page = requests.get(url)
             soup = BeautifulSoup(page.content, "html.parser")
             table = soup.findChildren('table')
@@ -28,12 +32,15 @@ for depart in allCourses:
                 rows = body[0].findChildren('tr', recursive=False)
                 cells = []
                 for row in rows:
-                    cells = cells + (row.select('td:not([align="right"])', recursive=False))
+                    cells = cells + \
+                        (row.select(
+                            'td:not([align="right"])', recursive=False))
                 for idx, cell in enumerate(cells):
                     if cell.has_attr('class'):
-                        allCourses[depart][courseCode]["section"][sectionNo]["schedule"][idx] =  cell.text if cell else "N/A"
+                        section["schedule"].append(
+                            {"slot": idx, "classroom": cell.text})
 
 
-file_path = (base_path / "../data/courses.json").resolve()
+file_path = (base_path / "../data/courses-extra.json").resolve()
 with open(file_path, "w+", encoding="utf-8") as f:
     json.dump(allCourses, f, ensure_ascii=False, indent=2)
