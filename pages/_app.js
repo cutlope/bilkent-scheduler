6ab9814c64d197b4e8f353d86390f1deec451aea
cutlope <abdullah77@outlook.com>
2022-08-17 02:24:21 +0500

    favicon fix

--- pages/_app.js
+++ pages/_app.js
@@ -32,23 +32,23 @@ function MyApp({ Component, pageProps }) {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="../public/favicons/apple-touch-icon.png"
+          href="apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="../public/favicons/favicon-32x32.png"
+          href="favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="../public/favicons/favicon-16x16.png"
+          href="favicon-16x16.png"
         />
         <link
           rel="manifest"
-          href="../public/favicons/site.webmanifest"
+          href="site.webmanifest"
         />
       </Head>
       <Script
